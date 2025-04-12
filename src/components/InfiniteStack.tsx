0e1f8eebb847e5739b8b27469242c74103d08e9f
Alfian Ramadani <Alfianr792@gmail.com>
2025-04-12 19:39:20 +0800

    stack align change

--- src/components/InfiniteStack.tsx
+++ src/components/InfiniteStack.tsx
@@ -2,22 +2,18 @@ import React from 'react';
 import { Icons1, Icons2 } from '../assets/stack/icon';
 import { Icon } from '../Types';
 
-const StackRight = ({ Icons }: { Icons: Icon[] }) => {
-  return (
-    <div className="flex gap-10 animate-infinite-scroll-left group-hover:animation-pause p-4">
-      {Icons?.map((icon, index) => (
-        <div key={index} className="flex flex-row justify-center gap-4 px-10 py-5 bg-card1 hover:ring-2 hover:ring-ring1 rounded-xl">
-          <div className="flex items-center w-12 h-12  md:w-16 md:h-16">
-            <img src={icon.src} alt={icon.alt} className="" />
-          </div>
-          <div className="flex flex-col justify-center">
-            <p className="text-primary font-medium text-sm">{icon.alt}</p>
-            <p className="text-secondary font-thin text-sm">{icon.desc}</p>
-          </div>
-        </div>
-      ))}
+const Stack = ({ Icons }: { Icons: Icon[] }) => {
+  return Icons?.map((icon, index) => (
+    <div key={index} className="flex flex-row justify-center gap-4 px-10 py-5 bg-card1 hover:ring-2 hover:ring-ring1 rounded-xl">
+      <div className="flex items-center w-12 h-12  md:w-16 md:h-16">
+        <img src={icon.src} alt={icon.alt} className="" />
+      </div>
+      <div className="flex flex-col justify-center">
+        <p className="text-primary font-medium text-sm">{icon.alt}</p>
+        <p className="text-secondary font-thin text-sm">{icon.desc}</p>
+      </div>
     </div>
-  );
+  ));
 };
 
 const InfiniteStack = () => {
@@ -29,12 +25,16 @@ const InfiniteStack = () => {
       </div>
       <div className="py-7">
         <div className="inline-flex w-full  flex-nowrap group relative overflow-hidden whitespace-nowrap [mask-image:_linear-gradient(to_right,_transparent_0,_white_128px,white_calc(100%-128px),_transparent_100%)]">
-          <StackRight Icons={Icons1} />
-          <StackRight Icons={Icons1} />
+          <div className="flex gap-10 animate-infinite-scroll-right group-hover:animation-pause p-4">
+            <Stack Icons={Icons1} />
+            <Stack Icons={Icons1} />
+          </div>
         </div>
         <div className="inline-flex w-full flex-nowrap group relative overflow-hidden whitespace-nowrap [mask-image:_linear-gradient(to_right,_transparent_0,_white_128px,white_calc(100%-128px),_transparent_100%)]">
-          <StackRight Icons={Icons2} />
-          <StackRight Icons={Icons2} />
+          <div className="flex gap-10 animate-infinite-scroll-left group-hover:animation-pause p-4">
+            <Stack Icons={Icons2} />
+            <Stack Icons={Icons2} />
+          </div>
         </div>
       </div>
     </div>
